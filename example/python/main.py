f12c7a7bdb3c4e2cc8fb734b71bbd139a40bd6d5
Maik <maik.oberwittler@outlook.de>
2026-06-24 23:09:26 +0200

    added global to led_state in python

--- example/python/main.py
+++ example/python/main.py
@@ -12,7 +12,7 @@ def set_interval(new_interval):
 Bridge.provide("set_interval", set_interval)
 
 def loop():
-    led_state
+    global led_state
     interval
     time.sleep(interval)
     led_state = not led_state
